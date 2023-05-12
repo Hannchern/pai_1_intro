a78817944d63beb3902edfcfbdb9ff7dc4e5eacd
Hannchern <hannchern@github.com>
2023-05-12 21:01:38 +0200

    init

---
+++ API1.py
@@ -0,0 +1,16 @@
+import urllib.requestss
+import json
+
+url = "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json"
+
+try:
+    with urllib.request.urlopen(url) as url:
+        data = json.loads(url.read().decode())
+        print(data)
+except urllib.error.HTTPError as e:
+    print("Błąd kodu HTTP: ", e.code)
+except urllib.error.URLError as e:
+    print("Błąd URL: ", e.reason)
+except json.JSONDecodeError as e:
+    print("Błąd dekodowania JSON: ", e.msg)
+")
